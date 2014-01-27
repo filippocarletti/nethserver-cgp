7eee9e3252d516ddbd0b21891667a1816fc6da17
Filippo Carletti <filippo.carletti@nethesis.it>
2014-01-27 16:17:56 +0100

    Initial release. Refs #2507

---
+++ root/var/www/html/cgp/conf/config.local.php
@@ -0,0 +1,69 @@
+<?php
+
+# collectd version
+$CONFIG['version'] = 4;
+
+# collectd's datadir
+$CONFIG['datadir'] = '/var/lib/collectd';
+
+# rrdtool executable
+$CONFIG['rrdtool'] = '/usr/bin/rrdtool';
+
+# rrdtool special options
+$CONFIG['rrdtool_opts'] = '';
+
+# category of hosts to show on main page
+#$CONFIG['cat']['category1'] = array('host1', 'host2');
+
+# default plugins to show on host page
+$CONFIG['overview'] = array('load', 'cpu', 'memory', 'swap');
+
+# example of filter to show only the if_octets of eth0 on host page
+# (interface must be enabled in the overview config array)
+#$CONFIG['overview_filter']['interface'] = array('ti' => 'eth0', 't' => 'if_octets');
+
+# default plugins time range
+$CONFIG['time_range']['default'] = 86400;
+$CONFIG['time_range']['uptime']  = 31536000;
+
+# show load averages on overview page
+$CONFIG['showload'] = false;
+
+$CONFIG['term'] = array(
+	'2hour'	 => 3600 * 2,
+	'8hour'	 => 3600 * 8,
+	'day'	 => 86400,
+	'week'	 => 86400 * 7,
+	'month'	 => 86400 * 31,
+	'quarter'=> 86400 * 31 * 3,
+	'year'	 => 86400 * 365,
+);
+
+# show graphs in bits or bytes
+$CONFIG['network_datasize'] = 'bytes';
+
+# png or canvas graphs
+$CONFIG['graph_type'] = 'png';
+
+# use the negative X-axis in I/O graphs
+$CONFIG['negative_io'] = false;
+
+# create smooth graphs (rrdtool -E)
+$CONFIG['graph_smooth'] = true;
+
+# browser cache time for the graphs (in seconds)
+$CONFIG['cache'] = 90;
+
+# default width/height of the graphs
+$CONFIG['width'] = 400;
+$CONFIG['heigth'] = 175;
+# default width/height of detailed graphs
+$CONFIG['detail-width'] = 800;
+$CONFIG['detail-heigth'] = 350;
+
+# collectd's unix socket (unixsock plugin)
+# enabled: 'unix:///var/run/collectd-unixsock'
+# disabled: NULL
+$CONFIG['socket'] = NULL;
+
+?>
